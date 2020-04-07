2cefd220d55594ffdc30390b88a404ba265547e4
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-07 15:06:37 +0530

    Counting Elements
    Given an integer array arr, count element x such that x + 1 is also in arr.
    
    If there're duplicates in arr, count them seperately.
    
     
    
    Example 1:
    
    Input: arr = [1,2,3]
    Output: 2
    Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
    Example 2:
    
    Input: arr = [1,1,3,3,5,5,7,7]
    Output: 0
    Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
    Example 3:
    
    Input: arr = [1,3,2,3,5,0]
    Output: 3
    Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
    Example 4:
    
    Input: arr = [1,1,2,2]
    Output: 2
    Explanation: Two 1s are counted cause 2 is in arr.

---
+++ 07.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countElements(int[] arr) {
+        
+        HashSet<Integer> h = new HashSet<> () ;
+        int count = 0 ;
+        int x = 0 ;
+        
+        for ( int i = 0 ; i < arr.length ; i++ ) {
+            
+            h.add( arr[i] ) ;
+            
+        }
+        
+        for ( int i = 0 ; i < arr.length ; i++ ) {
+            
+            if ( h.contains( arr[i] + 1 ) ){
+                
+                count++ ;
+            }
+        }        
+   
+        return count ;
+    }
+}
