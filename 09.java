0adb224caee48c8a2cd8ecccce9b9f3876cf3539
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-10 17:01:44 +0530

    Backspace String Compare
    Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
    Example 1:
    
    Input: S = "ab#c", T = "ad#c"
    Output: true
    Explanation: Both S and T become "ac".
    Example 2:
    
    Input: S = "ab##", T = "c#d#"
    Output: true
    Explanation: Both S and T become "".
    Example 3:
    
    Input: S = "a##c", T = "#a#c"
    Output: true
    Explanation: Both S and T become "c".
    Example 4:
    
    Input: S = "a#c", T = "b"
    Output: false
    Explanation: S becomes "c" while T becomes "b".

---
+++ 09.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1;
+        int j = T.length() - 1;
+        int skipS = 0; 
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j)) {
+                return false;
+            }
+
+            if (i >= 0 && j < 0 || i < 0 && j >= 0) {
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
